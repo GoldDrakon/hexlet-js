dc1cce0192c965883f1222ea431c68d1483bac6e
Ilya Ermakov <Ermakov_Ilia88@mail.ru>
2025-03-31 11:56:11 +0700

    add .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, Hexlet!");
+
+// По общепринятому соглашению, lodash импортируется под именем _
+const _ = require("lodash");
+
+// Пересечение массивов
+console.log(_.intersection([2, 1], [2, 3])); // => [2]
+
+// capitalize делает первую букву заглавной
+console.log(_.capitalize("hello"));
+console.log(_.last(['one', 'two']));
+
